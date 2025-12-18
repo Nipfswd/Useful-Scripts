53026417e724ca21e73d8fa092f3558ef6342239
Nipfswd <allaj0412@gmail.com>
2025-12-18 12:15:53 +0100

    added cf.sh

---
+++ cf.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# author: Noah Juopperi <noahjuopperi.contact@gmail.com>
+
+echo "-------------------------------------------------------"
+echo " Wassup, I create files for you when you're lazy."
+echo " I may not be the most advanced but my creator is good."
+echo "-------------------------------------------------------"
+echo
+
+# what's the name of the file
+read -p "What should we call your file? " filename
+
+echo "Great choice! We'll name it \"$filename\"."
+echo 
+
+# ask for the content
+echo "Now, let's put something inside your file."
+read -p "Type the first line of text: " filecon
+
+# i ran out of comments
+echo "$filecon" > "$filename"
+
+# because the user is probably a bit drunk since they forgot touch exists
+# we'll give them some feedback
+echo
+echo "All is done! I just created \"$filename\" for you."
+echo "Here's what it looks like inside:"
+echo "-------------------------------------------------"
+cat "$filename"
+echo "-------------------------------------------------"
+
+# closing message
+echo
+echo "Thanks for using me! I'll be ready next time."
